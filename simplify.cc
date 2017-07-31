e301fbf082845ac7d256920e7585a56d2f58c8a8
TTeun <teunverstraaten@gmail.com>
2017-07-31 16:45:49 +0200

    Equation is now simplified via collapse_visitor

--- simplify.cc
+++ simplify.cc
@@ -1,4 +1,5 @@
 #include "simplify.h"
+#include <cmath>
 
 namespace client {
 
@@ -25,6 +26,25 @@ namespace client {
     }
   }
 
+  double get_val(double val, UN_OP t){
+    switch (t) {
+      case UN_OP::SIN:
+        return sin(val);
+        break;
+      case UN_OP::COS:
+        return cos(val);
+        break;
+      case UN_OP::TAN:
+        return tan(val);
+        break;
+      case UN_OP::EXP:
+        return exp(val);
+        break;
+      case UN_OP::MIN:
+        return -val;
+        break;
+    }
+  }
 
   collapsed_type collapse_visitor::operator()(nil & t) const {
     return collapsed_type();
@@ -36,6 +56,14 @@ namespace client {
     return collapsed_type();
    }
   collapsed_type collapse_visitor::operator()(binary_operation & t) const {
+    collapsed_type c = boost::apply_visitor(collapse_visitor(), t.left.type);
+    if (c.can_collapse)
+      t.left.type = c.value;
+
+    c = boost::apply_visitor(collapse_visitor(), t.right.type);
+    if (c.can_collapse)
+      t.right.type = c.value;
+
     num_type left = boost::apply_visitor(num_visitor(), t.left.type);
     num_type right = boost::apply_visitor(num_visitor(), t.right.type);
     if (left && right)
@@ -44,6 +72,14 @@ namespace client {
     return collapsed_type();
   }
   collapsed_type collapse_visitor::operator()(unary_operation & t) const {
+    collapsed_type c = boost::apply_visitor(collapse_visitor(), t.tree.type);
+    if (c.can_collapse)
+      t.tree.type = c.value;
+
+    num_type tree = boost::apply_visitor(num_visitor(), t.tree.type);
+    if (tree)
+      return collapsed_type(get_val(tree.value, t.tag));
+
     return collapsed_type();
   }
 
