e23ad43c56fffd1a89cee155b04fd3f03549e462
TTeun <teunverstraaten@gmail.com>
2017-08-01 23:44:05 +0200

    evaluate nu chill, hij tekent ook nu grafieken

---
+++ equation/untitled/expression/linearspan.cpp
@@ -0,0 +1,6 @@
+#include "linearspan.h"
+
+LinearSpan::LinearSpan()
+{
+
+}
---
+++ equation/untitled/expression/linearspan.h
@@ -0,0 +1,11 @@
+#ifndef LINEARSPAN_H
+#define LINEARSPAN_H
+
+
+class LinearSpan
+{
+public:
+  LinearSpan();
+};
+
+#endif // LINEARSPAN_H
